57c1a5dbe7c98811eae5db8218dca6ca641de339
royvanderzon <royvanderzon@gmail.com>
2017-06-28 11:00:46 +0200

    vervelend nav dingentje bovenaan

--- public/cms/js/curriculum.js
+++ public/cms/js/curriculum.js
@@ -1,31 +1,3 @@
-//sendData post:
-// //1# data
-// //2# succes callback
-// //3# failure callback
-// sendData(data,function(){
-//     // success
-//     $('#status_'+data.key).removeClass('red-text');
-//     $('#status_'+data.key).addClass('green-text');
-//     $('#status_'+data.key).html('checkmark');
-
-//     $('#status_'+data.key).tooltip({
-//         delay : 50,
-//         tooltip : 'Saved',
-//         position : 'right'
-//     });
-// },function(){
-//     // failure
-//     $('#status_'+data.key).removeClass('green-text');
-//     $('#status_'+data.key).addClass('red-text');
-//     $('#status_'+data.key).html('close');
-
-//     $('#status_'+data.key).tooltip({
-//         delay : 50,
-//         tooltip : 'No connection',
-//         position : 'right'
-//     });
-// });
-
 function imageHandler(image, callback) {
     var data = new FormData();
     data.append('image', image);
@@ -453,6 +425,10 @@ $(document).ready(function() {
     }
 
     makeSortable()
+
+    $(".edit_flex_sub_column").click(function(){
+        console.log($(this))
+    })
 
     // $( ".draggable" ).draggable({
     //   connectToSortable: "#sortable",
